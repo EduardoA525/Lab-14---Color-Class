cd84a913f2584c3eada09b57e0bc772b1e7c3c75
EduardoA525 <eavila913@insite.4cd.edu>
2026-09-25 16:18:08 -0700

    setters n getters

--- Lab14.cpp
+++ Lab14.cpp
@@ -14,3 +14,33 @@ Commit your code every ten minutes while working. Set a timer.
 //Eduardo Avila
 //COMSC - 210 - 5293
 //Lab 14 - Color Class
+
+#include <iostream>
+using namespace std;
+
+//Color class
+class Color {
+
+private:
+    int red;
+    int green;
+    int blue;
+
+public: //Setters and Getters
+    int getRed()        { return red; }
+    void setRed(int r)  { red = r; }
+
+    int getGreen()        { return green; }
+    void setGreen(int g)  { green = g; }
+
+    int getBlue()        { return blue; }
+    void setBlue(int b)  { blue = b; }
+
+    
+
+
+
+
+
+
+};
